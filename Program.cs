0928ff696b1f8397151057edd9c26eac0ae278cf
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-14 14:40:08 +0530

    Add : All Logical Program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Prog...");
+        }
+    }
+}
